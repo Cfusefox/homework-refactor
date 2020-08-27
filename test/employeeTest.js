3091f59e99d0a51b0e9f681b78b06e4779f07021
confusefox <1036674617@qq.com>
2020-08-28 00:54:56 +0800

    [York] feature: add employee test case2

--- test/employeeTest.js
+++ test/employeeTest.js
@@ -1,5 +1,6 @@
 const employeeTest = require('ava');
-const { Employee } = require('../src/employee')
+const { Employee } = require('../src/employee');
+const employee = require('../src/employee');
 
 employeeTest('employee test case1', t => {
     try {
@@ -7,5 +8,14 @@ employeeTest('employee test case1', t => {
         t.fail()
     } catch(e) {
         t.is('Employee cannot be of type test', e.message)
+    }
+})
+
+employeeTest('employee test case2', t => {
+    try {
+        let employee = new Employee('fox', 'manager')
+        t.is('fox (manager)', employee.toString())
+    } catch(e) {
+
     }
 })
