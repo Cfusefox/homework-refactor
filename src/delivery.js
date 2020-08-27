c0fc3af718b031c1a167b5b4d06df4ccb779443b
confusefox <1036674617@qq.com>
2020-08-28 01:55:52 +0800

    [York] refactor: extract function notInIsRush

--- src/delivery.js
+++ src/delivery.js
@@ -18,29 +18,33 @@ const inIsRush = (anOrder) => {
   return deliveryTime
 }
 
+const notInIsRush = (anOrder) => {
+  let deliveryTime;
+  if ([
+    'MA',
+    'CT',
+    'NY',
+  ].includes(anOrder.deliveryState)) {
+    deliveryTime = 2;
+  }
+  else if ([
+    'ME',
+    'NH',
+  ].includes(anOrder.deliveryState)) {
+    deliveryTime = 3;
+  }
+  else {
+    deliveryTime = 4;
+  }
+  return deliveryTime
+}
+
 function deliveryDate (anOrder, isRush) {
   if (isRush) {
     return anOrder.placedOn.plusDays(1 + inIsRush(anOrder));
   }
   else {
-    let deliveryTime;
-    if ([
-      'MA',
-      'CT',
-      'NY',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
-    }
-    else if ([
-      'ME',
-      'NH',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 3;
-    }
-    else {
-      deliveryTime = 4;
-    }
-    return anOrder.placedOn.plusDays(2 + deliveryTime);
+    return anOrder.placedOn.plusDays(2 + notInIsRush(anOrder));
   }
 }
 
