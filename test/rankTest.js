b806601ebb3f0b96117cd8664f521954472bd4d1
confusefox <1036674617@qq.com>
2020-08-27 20:29:35 +0800

    [York] feature: add rank test case 1

--- test/rankTest.js
+++ test/rankTest.js
@@ -1,11 +1,27 @@
 const rankTest = require('ava');
+const {rating} = require('../src/rank')
 
-rankTest('foo', t => {
-  t.pass();
-});
-
-rankTest('bar', async t => {
-  const bar = Promise.resolve('bar');
-  t.is(await bar, 'bar');
-});
-
+rankTest('rank test case 1', t => {
+  const voyage = {
+    zone: 'west-indies',
+    length: 10,
+  };
+  const history = [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },{
+      zone: 'china',
+      profit: -2,
+    },
+    {
+      zone: 'west-africa',
+      profit: 7,
+    },
+  ];
+  const myRating = rating(voyage, history);
+  t.is(`myRating: ${myRating}`, 'myRating: B')
+})
