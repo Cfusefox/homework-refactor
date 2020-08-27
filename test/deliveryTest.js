ac441f1b1063b5eebb64f0318c0725aac56e9fce
confusefox <1036674617@qq.com>
2020-08-28 01:52:25 +0800

    [York] feature: add delivery test case2

--- test/deliveryTest.js
+++ test/deliveryTest.js
@@ -13,3 +13,16 @@ deliveryTest('delivery test case1', t => {
     const isRush = true
     t.is(deliveryDate(anOrder, isRush), 2)
 })
+
+deliveryTest('delivery test case2', t => {
+    const anOrder = {
+        deliveryState: 'MA',
+        placedOn: {
+            plusDays: (num) => {
+                return num;
+            }
+        }
+    }
+    const isRush = false
+    t.is(deliveryDate(anOrder, isRush), 4)
+})
