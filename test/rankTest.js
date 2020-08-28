b74e1e269038a3012a99a42fa82fe54ceeb88b53
confusefox <1036674617@qq.com>
2020-08-28 22:52:57 +0800

    [York] feature: add rank test case 6

--- test/rankTest.js
+++ test/rankTest.js
@@ -97,3 +97,22 @@ rankTest('rank test case 5', t => {
   const myRating = rating(voyage, history);
   t.is(`myRating: ${myRating}`, 'myRating: A')
 })
+
+rankTest('rank test case 6', t => {
+  const voyage = {
+    zone: 'west-indies',
+    length: 10,
+  };
+  const history = [
+    {
+      zone: 'west-indies',
+      profit: -5,
+    },
+    {
+      zone: 'china',
+      profit: 12,
+    }
+  ];
+  const myRating = rating(voyage, history);
+  t.is(`myRating: ${myRating}`, 'myRating: B')
+})
